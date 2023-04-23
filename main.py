c180b6b3646410af9d198e5ee5f15fd59f27a03f
PeroxideParadox <118070626+PeroxideParadox@users.noreply.github.com>
2023-04-23 14:43:53 +0530

    Update main.py

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ import requests
 from streamlit_lottie import st_lottie
 import json
 
-# Load the pre-trained car classifier file
+
 st.set_page_config(page_title="My Webpage", page_icon=":tada:", layout="wide")
 
 
@@ -57,7 +57,7 @@ if choice=="CarDetection":
 
             cap = cv2.VideoCapture("temp_video.mp4")
 
-            # Loop through the frames of the video
+            
             while True:
                 # Read a frame from the video stream
                 ret, frame = cap.read()
@@ -72,27 +72,27 @@ if choice=="CarDetection":
                 # Detect cars in the frame
                 cars = car_cascade.detectMultiScale(rgb_frame, scaleFactor=1.1, minNeighbors=5)
 
-                # Draw rectangles around the detected cars
+               
                 for (x, y, w, h) in cars:
                     cv2.rectangle(rgb_frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
 
-                # Display the frame in the Streamlit window
+                
                 video_placeholder.image(rgb_frame, channels="RGB")
 
-            # Release the VideoCapture object and delete the temporary file
+            
             cap.release()
             os.remove("temp_video.mp4")
     with right_column:
         st_lottie(lottie_coding,height="10",key="coding")
-    #st_lottie(lottie_coding1,height="100",key="coding1")
+    
 
 elif choice=="Face Detection":
      with left_column:
          
-        # Create a file uploader widget
+        
         video_file = st.file_uploader("Upload a video file", type=["mp4", "avi"])
 
-        # Check if a video file was uploaded
+        
         if video_file is not None:
             # Read the contents of the video file
             video_bytes = video_file.read()
@@ -101,7 +101,7 @@ elif choice=="Face Detection":
             with open("temp_video.mp4", "wb") as f:
                 f.write(video_bytes)
 
-            # Create a placeholder for the video
+            
             video_placeholder = st.empty()
 
             # Open the video file using VideoCapture
@@ -119,17 +119,17 @@ elif choice=="Face Detection":
                 # Convert the frame from BGR to RGB format
                 rgb_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
 
-                # Detect cars in the frame
+                
                 cars = face_data.detectMultiScale(rgb_frame, scaleFactor=1.1, minNeighbors=5)
 
-                # Draw rectangles around the detected cars
+                
                 for (x, y, w, h) in cars:
                     cv2.rectangle(rgb_frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
 
                 # Display the frame in the Streamlit window
                 video_placeholder.image(rgb_frame, channels="RGB")
 
-            # Release the VideoCapture object and delete the temporary file
+           
             cap.release()
             os.remove("temp_video.mp4")
      with right_column:
